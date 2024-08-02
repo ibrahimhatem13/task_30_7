f5e61f5be0d060f7319d8abfec3a8fe61baf329f
ibrahimhatem13 <ibrahimhatem135@gmail.com>
2024-08-02 12:23:02 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
+
 <?php
 $the_name=null;
+
 if($_SERVER['REQUEST_METHOD']=="POST"){
     $the_name = $_POST['name'];
     $the_id = $_POST['id'];
     $the_department = $_POST['department'];
     $the_classroom = $_POST['classroom'];
     $the_gpa = $_POST['gpa'];
+    
     echo "<table style='width:80%; text-align:center; border-spacing:2px; margin:15px' >
             <tr style='background-color:#75b798; color:#eee; font-weight:bold; padding:10px;'>
                 <th>Student Name</th>
@@ -43,7 +46,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
         height: 100vh;
         padding-left:20px;">
         <h2 style="font-size: 45px; margin-bottom: 10px;font-family:'Gill Sans';">Welcome</h2>
-        <label style="width:120px" for="">Student Name</label>
+        <label style="width:120px;font-weight:bold" for="">Student Name</label>
+     
         <input style="height: 40px;
             width: 60%;
             padding: 0 28px;
@@ -53,7 +57,8 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             font-size:18px;
             background-color: initial;"  name="name" class="userName" placeholder="Student Name" type="text">
         <br/>
-        <label style="width:120px" for="">ID</label>
+        <label style="width:120px;font-weight:bold" for="">ID</label>
+        
         <input 
         style="height: 40px;
             width: 60%;
@@ -64,7 +69,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             font-size:18px;
             background-color: initial;" name="id" class="id" placeholder="ID" type="number"> 
         <br/>
-        <label style="width:120px" class="forget">Classroom</label>
+        <label style="width:120px;font-weight:bold" class="forget">Classroom</label>
         <select name="classroom" style="height: 40px;
             width: 60%;
             padding: 0 28px;
@@ -82,7 +87,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             </optgroup>
         </select>
         <br/>
-        <label style="width:120px" class="forget">Department</label>
+        <label style="width:120px ;font-weight:bold" class="forget">Department</label>
         <select style="height: 40px;
             width: 60%;
             padding: 0 28px;
@@ -92,16 +97,19 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             font-size:18px;
             background-color: initial;" class="department"  name="department" >
             <optgroup label="Select Department">
+
                 <option value="Communication">Communication</option>
                 <option value="Computer">Computer</option>
                 <option value="Electronics">Electronics</option>
-                <option value="Network">Network</option>
+                <option value="Network And Security">Network And Security</option>
                 <option value="Power">Power</option>
                 <option value="Control">Control</option>
+                
             </optgroup>
         </select>
         <br/>
-        <label style="width:120px" class="forget">GPA</label>
+        <label style="width:120px ; font-weight:bold" class="forget">GPA</label>
+       
         <input 
         style="height: 40px;
             width: 60%;
@@ -110,7 +118,7 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             border: 1px #6c14d0 solid;
             margin: 5px 0;
             font-size:18px;
-            background-color: initial;" name="gpa"  class="gpa" placeholder="GPA" type="number">
+            background-color: initial;" name="gpa" step="0.01" class="gpa" placeholder="GPA" type="number">
         <br/>
         <input 
         style="height: 40px;
@@ -119,7 +127,9 @@ if($_SERVER['REQUEST_METHOD']=="POST"){
             border: 1px #6c14d0 solid;
             margin: 5px 122px;
             font-size:18px;
-            background-color: initial;" type="submit">    
+            background-color: initial;" type="submit">
+            
     </form>
+    
 </body>
 </html>
